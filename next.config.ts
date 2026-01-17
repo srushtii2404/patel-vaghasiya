c512a7713843e30efb3bc8d6b80c476a0f456203
Srushti Gohel <srushtigohel2404@gmail.com>
2026-01-17 23:10:52 +0530

    depppp

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/**",
       },
     ],
   },
